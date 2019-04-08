509f18e90f3fe2724542d689f42d66c9b6cd9889
Mustafa Yıldırım <myldrm1025@gmail.com>
2019-04-08 17:08:44 +0300

    manipulate func added

--- mockservice/src/main/java/org/exchangerate/com/outerws/ProcessData.java
+++ mockservice/src/main/java/org/exchangerate/com/outerws/ProcessData.java
@@ -2,6 +2,8 @@ package org.exchangerate.com.outerws;
 
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -22,7 +24,7 @@ public class ProcessData {
 		setKeys();
 		JSONObject rates = data.getRequestResponse();
 		setOutput(rates);
-		System.out.println(output.toString());
+		System.out.println(rates);
 		
 		}
 	private void setKeys(){
@@ -55,6 +57,8 @@ public class ProcessData {
 		    	JSONObject temp2 = new JSONObject();
 		    	temp2.put("sell", temp.get(elements.get("sell")));
 		    	object = temp2;
+		
+		
 		    }
 		    
 		}
@@ -62,4 +66,36 @@ public class ProcessData {
 		output = object;
 		
 	}
+	private void manipulateDelay() throws InterruptedException{
+		if( Math.random() <= (faultmodel.get("delayprobability").doubleValue()/100)) {
+			TimeUnit.MILLISECONDS.sleep((long) faultmodel.get("delayamount").doubleValue());
+			}
+	}
+	private String manipulateSell(String value){
+		Random rand = new Random();
+		double result = Double.parseDouble(value);
+		if( Math.random() <= (faultmodel.get("spositiveProb").doubleValue()/100)) {
+			  int positive = rand.nextInt(faultmodel.get("spositiveRange").intValue());
+			  result = result + (result*positive/100);
+			}
+		if( Math.random() <= (faultmodel.get("snegativeProb").doubleValue()/100)) {
+			  int negative = rand.nextInt(faultmodel.get("snegativeRange").intValue());
+			  result = result - (result*negative/100);
+			}
+		return Double.toString(result);
+	}
+	private String manipulateBuy(String value){
+		Random rand = new Random();
+		double result = Double.parseDouble(value);
+		if( Math.random() <= (faultmodel.get("bpositiveProb").doubleValue()/100)) {
+			  int positive = rand.nextInt(faultmodel.get("bpositiveRange").intValue());
+			  result = result + (result*positive/100);
+			}
+		if( Math.random() <= (faultmodel.get("bnegativeProb").doubleValue()/100)) {
+			  int negative = rand.nextInt(faultmodel.get("bnegativeRange").intValue());
+			  result = result - (result*negative/100);
+			}
+		return Double.toString(result);
+	}
+	
 }
